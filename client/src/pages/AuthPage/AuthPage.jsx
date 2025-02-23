9aae7473c5ad1a14793acfdf903d52a24bda57ef
RuslanGon <ruslan441006@gmail.com>
2025-02-23 14:39:12 +0200

    wer

--- client/src/pages/AuthPage/AuthPage.jsx
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -22,6 +22,25 @@ const AuthPage = () => {
             <a className="btn-outline btn-reg" href="/">Нет аккаунта?</a>
           </div>
         </form>
+
+
+        <h3>Регистрация</h3>
+        <form className="form form-login">
+          <div className="row">
+            <div className="input-field col s12">
+              <input className="validate" type="email" name="email" id="email" />
+              <label htmlFor="email">Введите email</label>
+            </div>
+            <div className="input-field col s12">
+              <input className="validate" type="password" name="password" id="password" />
+              <label htmlFor="password">Введите пароль</label>
+            </div>
+          </div>
+          <div className="row">
+            <button className="waves-effect waves-light btn blue">Регистрация</button>
+            <a className="btn-outline btn-reg" href="/">Уже есть аккаунт?</a>
+          </div>
+        </form>
       </div>
     </div>
   );
