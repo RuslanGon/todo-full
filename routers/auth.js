46f7ee0a9ed87865abedd7d675a200ffb329ce15
RuslanGon <ruslan441006@gmail.com>
2025-02-23 15:03:01 +0200

    wee

--- routers/auth.js
+++ routers/auth.js
@@ -4,16 +4,19 @@ import User from "../models/User.js";
 
 const router = new Router()
 
-router.post('/registration',  async (req, res) => {
-try {
-    const {email, password} = req.body
-    const isUsed = await User.findOne({email})
-    if(isUsed) {
-        return res.staus(300).json({message: 'Данный email уже занят'})
+router.post("/registration", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const isUsed = await User.findOne({ email });
+    if (isUsed) {
+      return res.staus(300).json({ message: "Данный email уже занят" });
     }
-} catch (error) {
+    const user = new User([email, password]);
+    await user.save();
+    res.status(201).json({message: 'Пользователь создан'})
+  } catch (error) {
     console.log(error);
-}
-})
+  }
+});
 
 export default router
