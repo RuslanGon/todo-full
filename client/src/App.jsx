c9c7161e8688943592e6b340434835bb5895fb15
RuslanGon <ruslan441006@gmail.com>
2025-02-23 15:31:56 +0200

    wer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
-import './App.scss'
-import Navbar from './components/Navbar/Navbar.jsx'
-import AuthPage from './pages/AuthPage/AuthPage.jsx'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
+import "./App.scss";
 
 function App() {
-
   return (
     <div>
       <Navbar />
-      <AuthPage />
+      <div className="container">
+        <Routes>
+        <Route path="/" element={<h1>Главная страница</h1>} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/registration" element={<RegisterPage />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,30 +1,27 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   return (
     <div>
-        <div>
-    <h3>Авторизация</h3>
-    <form className="form form-login">
-      <div className="row">
-        <div className="input-field col s12">
-          <input className="validate" type="email" name="email" id="email-login" />
-          <label htmlFor="email-login">Введите email</label>
+      <h3>Авторизация</h3>
+      <form className="form form-login">
+        <div className="row">
+          <div className="input-field col s12">
+            <input className="validate" type="email" name="email" id="email-login" />
+            <label htmlFor="email-login">Введите email</label>
+          </div>
+          <div className="input-field col s12">
+            <input className="validate" type="password" name="password" id="password-login" />
+            <label htmlFor="password-login">Введите пароль</label>
+          </div>
         </div>
-        <div className="input-field col s12">
-          <input className="validate" type="password" name="password" id="password-login" />
-          <label htmlFor="password-login">Введите пароль</label>
+        <div className="row">
+          <button className="waves-effect waves-light btn blue">Войти</button>
+          <Link className="btn-outline btn-reg" to="/registration">Нет аккаунта?</Link>
         </div>
-      </div>
-      <div className="row">
-        <button className="waves-effect waves-light btn blue">Войти</button>
-        <Link className="btn-outline btn-reg" to="/registration">Нет аккаунта?</Link>
-      </div>
-    </form>
-  </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
