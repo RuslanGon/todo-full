96beb8d1c7cf09be59df9d0e2d2d54e45947537c
RuslanGon <ruslan441006@gmail.com>
2025-02-24 17:59:01 +0200

    wewe

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,60 +1,3 @@
-// import { Link } from "react-router-dom";
-// import './Login.scss'
-// import { useState, useContext } from "react";
-// import axios from "axios";
-// import { AuthContext } from "../context/AuthContext.js";
-
-
-
-// const LoginPage = () => {
-
-//   const {login} = useContext(AuthContext)
-
-//   const [form, setForm] = useState({ email: '', password: ''})
-
-//   const changeHandler = (event) => {
-//     setForm({...form, [event.target.name]: event.target.value})
-//     console.log(form);
-//   }
-
-//   const loginHandler = async () => {
-// try {
-//  await axios.post('/api/auth/login', {...form}, {
-//     headers: {'Content-Type': 'application/json'}
-//   })
-//   .then(response => {
-//     login(response.data.token, response.data.userId)
-//   })
-// } catch (error) {
-//   console.log(error);
-// }
-//   }
- 
-
-//   return (
-//     <div className="div">
-//       <h3>Авторизация</h3>
-//       <form className="form form-login" onSubmit={e => e.preventDefault()}>
-//         <div className="row">
-//           <div className="input-field col s12">
-//             <input onChange={changeHandler}  className="validate" type="email" name="email" id="email-login" />
-//             <label htmlFor="email-login">Введите email</label>
-//           </div>
-//           <div className="input-field col s12">
-//             <input onChange={changeHandler} className="validate" type="password" name="password" id="password-login" />
-//             <label htmlFor="password-login">Введите пароль</label>
-//           </div>
-//         </div>
-//         <div className="row">
-//           <button onClick={loginHandler} className="waves-effect waves-light btn blue">Войти</button>
-//           <Link className="btn-outline btn-reg" to="/registration">Нет аккаунта?</Link>
-//         </div>
-//       </form>
-//     </div>
-//   );
-// };
-
-// export default LoginPage;
 
 import { Link, useNavigate } from "react-router-dom";
 import './Login.scss'
@@ -69,7 +12,7 @@ const LoginPage = () => {
 
   useEffect(() => {
     if (isLogin) {
-      navigate('/');  // Если пользователь авторизован, перенаправляем на главную
+      navigate('/');  
     }
   }, [isLogin, navigate]);
 
@@ -84,7 +27,7 @@ const LoginPage = () => {
       });
 
       login(response.data.token, response.data.userId);
-      navigate('/'); // Перенаправление на главную после входа
+      navigate('/'); 
 
     } catch (error) {
       console.log(error.response?.data?.message || "Ошибка входа");
