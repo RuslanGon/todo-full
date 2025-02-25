bcd5a4d1ce3252e539bfc9a9f02829554a53499f
RuslanGon <ruslan441006@gmail.com>
2025-02-25 10:13:51 +0200

    wer

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,16 +1,33 @@
+import { useContext } from 'react'
 import './Navbar.scss'
+import { AuthContext } from '../../context/AuthContext.js'
 
 const Navbar = () => {
+
+const {islogin, logout} = useContext(AuthContext)
+
   return (
     <nav>
-    <div className="nav-wrapper navbar blue">
-      <a href="/" className="brand-logo">Todo app</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="/">Войти</a></li>
-      </ul>
-    </div>
-  </nav>
-  )
+      <div className="nav-wrapper navbar blue">
+        <a href="/" className="brand-logo">
+          Todo app
+        </a>
+        {islogin ? (
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a onClick={logout} href="/">Выйти</a>
+            </li>
+          </ul>
+        ) : (
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <a href="/">Войти</a>
+            </li>
+          </ul>
+        )}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
