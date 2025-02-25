60ca1f82c3671b08105ef093e1943daeb79ca743
RuslanGon <ruslan441006@gmail.com>
2025-02-25 10:33:14 +0200

    wer

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,16 @@
-import { useContext } from 'react'
-import './Navbar.scss'
-import { AuthContext } from '../../context/AuthContext.js'
+import { useContext } from "react";
+import "./Navbar.scss";
+import { AuthContext } from "../../context/AuthContext.js";
 
 const Navbar = () => {
-
-const {islogin, logout} = useContext(AuthContext)
+  const { isLogin, logout } = useContext(AuthContext); 
 
   return (
     <nav>
       <div className="nav-wrapper navbar blue">
-        <a href="/" className="brand-logo">
-          Todo app
-        </a>
-        {islogin ? (
+        <a href="/" className="brand-logo">Todo app</a>
+        {isLogin ? (
+
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
               <a onClick={logout} href="/">Выйти</a>
@@ -21,13 +19,13 @@ const {islogin, logout} = useContext(AuthContext)
         ) : (
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <a href="/">Войти</a>
+              <a href="/login">Войти</a>
             </li>
           </ul>
         )}
       </div>
     </nav>
   );
-}
+};
 
-export default Navbar
+export default Navbar;
